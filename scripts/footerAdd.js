123377c64ecd4f51b5c03fa3f3caaf417bc072b0
patosambucari <patosambucari@gmail.com>
2023-08-28 21:00:05 -0300

    Frontend al 90%

---
+++ scripts/footerAdd.js
@@ -0,0 +1,5 @@
+
+        const footer = document.createElement("div");
+        document.getElementsByClassName("rw-sender")[0].appendChild(footer);
+        const textNode = document.createTextNode("Imagine One Live Chat");
+        footer.appendChild(textNode);
