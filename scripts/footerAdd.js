3e4fbc0dd2a5167d8f5bfb1429f81fdc6c5e4037
patosambucari <patosambucari@gmail.com>
2023-08-29 00:26:26 -0300

    Frontend Terminado

--- scripts/footerAdd.js
+++
@@ -1,5 +0,0 @@
-
-        const footer = document.createElement("div");
-        document.getElementsByClassName("rw-sender")[0].appendChild(footer);
-        const textNode = document.createTextNode("Imagine One Live Chat");
-        footer.appendChild(textNode);
---
+++ scripts/webchat-script.js
@@ -0,0 +1,28 @@
+        export default function () {
+        let e = document.createElement("script"),
+            t = document.head || document.getElementsByTagName("head")[0];
+        localStorage.clear(); //Borrar Historial
+        (e.src =
+            "https://cdn.jsdelivr.net/npm/rasa-webchat@1.x.x/lib/index.js"),
+            (e.async = !0),
+            (e.onload = () => {
+                window.WebChat.default(
+                    {
+                        initPayload: '/start',
+                        customData: { language: "en" },
+                        socketUrl: "http://localhost:5005",
+                        title: 'Imagine One',
+                        subtitle: '🟢 Disponible',
+                        storage: 'session',
+                        profileAvatar: "./images/logoi1.png",
+                        openLauncherImage: "./images/abrirChat.png",
+                        closeImage: "./images/cerrarChat.png",
+                        inputTextFieldHint: "Escribe un mensaje...",
+                        connectingText: "Intentando conectar...",
+                        hideWhenNotConnected: false
+                    },
+                    null
+                );
+            }),
+        t.insertBefore(e, t.firstChild);
+}
